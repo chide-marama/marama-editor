ed5932b26299f998a316ed73cf38ad255abb5f43
LuneCoding <erik.vanlune@gmail.com>
2018-04-23 10:44:22 +0200

    Added a test and a function to serve as example.
    Signed-off-by: LuneCoding <erik.vanlune@gmail.com>

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,4 +4,9 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello, Marama!")
   }
+  def Hello() : String = {
+    val Hello = "Hello"
+    //Return isn't necessary.
+    Hello
+  }
 }
